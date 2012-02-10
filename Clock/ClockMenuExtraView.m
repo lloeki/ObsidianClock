8136a3067ea6f0a8391ae9089fa0fce8ca8314be
Loic Nageleisen <loic.nageleisen@gmail.com>
2012-02-10 10:29:19 +0100

    correct background color

--- Clock/ClockMenuExtraView.m
+++ Clock/ClockMenuExtraView.m
@@ -16,13 +16,23 @@
 - (void)drawRect:(NSRect)rect
 {
     NSColor *textColor;
-    //NSColor *color;
+    NSColor *backColor;
     if([_menuExtra isMenuDown]) {
         textColor = [NSColor selectedMenuItemTextColor];
-        //color = [NSColor selectedMenuItemColor];
+        switch ([NSColor currentControlTint]) {
+            case NSGraphiteControlTint:
+                backColor = [NSColor colorWithDeviceRed:0.3333333333333333f green:0.36470588235294116f blue:0.40784313725490196f alpha:1.0f];
+                break;
+            case NSBlueControlTint:
+                backColor = [NSColor colorWithDeviceRed:0.18823529411764706f green:0.24705882352941178f blue:0.9176470588235294f alpha:1.0f];
+                break;
+            default:
+                backColor = [NSColor colorWithDeviceRed:0.0f green:0.0f blue:0.0f alpha:0.0f];
+                break;
+        }
     } else {
         textColor =  [NSColor lightGrayColor];
-        //color = [NSColor blackColor];
+        backColor = [NSColor colorWithDeviceRed:0.0f green:0.0f blue:0.0f alpha:0.0f];
     }
     
     // Disable LCD subpixel smoothing
@@ -30,10 +40,12 @@
     
     NSDictionary *attr = [NSDictionary dictionaryWithObjectsAndKeys: font, NSFontAttributeName,
                                                                      textColor, NSForegroundColorAttributeName,
-                                                                     //color, NSBackgroundColorAttributeName,
+                                                                     backColor, NSBackgroundColorAttributeName,
                                                                      nil];
     
-    // Draw    
+    // Draw
+    [backColor set];
+    [NSBezierPath fillRect:rect];
     NSRect smallerRect = NSMakeRect(1, 0, rect.size.width-1, rect.size.height-1);
     [text drawInRect:smallerRect withAttributes:attr];
 }
